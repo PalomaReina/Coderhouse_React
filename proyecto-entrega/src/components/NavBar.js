466a487c6e8534a9b925e3cd841801e1e9a160bd
PalomaReina <palomareina18@gmail.com>
2023-02-01 21:04:53 -0300

    .

--- proyecto-entrega/src/components/NavBar.js
+++ proyecto-entrega/src/components/NavBar.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react'
-import {Navbar, Nav, Container} from 'react-bootstrap'
-import {BrowserRouter as Router, Switch, Route,Link} from 'react-router-dom'
-  
-export default class NavBar extends Component {
-  render() {
-    return (
-    <Router>
-    <div>
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">La Tienda de Oak!</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to={"/home"}>Inicio</Nav.Link>
-            <Nav.Link as={Link} to={"/pokebolas"}>Pokebolas</Nav.Link>
-            <Nav.Link href="#link">item2</Nav.Link>
-            <Nav.Link href="#link">item3</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </div>
-    </Router>
-    );
-  };
-};
+import React, { Component } from 'react';
+import { Navbar, Nav , Container} from 'react-bootstrap';
+
+export default class NavBar extends Component{
+  render(){
+      return(
+          <div>
+          <Navbar bg="light" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">La tienda de Oak!</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="/home">Home</Nav.Link>
+                  <Nav.Link href="/pokebolas">Pokebolas</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+          </div>
+    )
+  }
+}
+
---
+++ proyecto-entrega/src/components/pokebolas.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class pokebolas extends Component{
+    render(){
+        return(
+            <div>
+            <h1>pokebolas</h1> 
+            </div>
+            )
+    }
+}
