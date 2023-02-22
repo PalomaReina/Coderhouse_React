600b736f3bdbde2a4cd54c20e546a19232b3d251
PalomaReina <palomareina18@gmail.com>
2023-02-21 21:53:16 -0300

    .
    .

--- proyecto-entrega/src/App.js
+++ proyecto-entrega/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import pokebolas from './components/pokebolas';
+
 
 
 
--- proyecto-entrega/src/components/CartWidget.js
+++ proyecto-entrega/src/components/CartWidget.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CartWidget({cantidadCarrito}) {
 return (
     <div>
-        <button class="btn btn-dark">Carrito</button> 
+        <button class="btn btn-dark"><i class="bi bi-cart"></i></button> 
         <p>{cantidadCarrito}</p>
     </div>
 )
