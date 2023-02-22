35e4f975b64d637a049bb63e265419231863256a
PalomaReina <palomareina18@gmail.com>
2023-02-21 21:09:55 -0300

    .
    .

--- proyecto-entrega/src/App.js
+++ proyecto-entrega/src/App.js
@@ -1,15 +1,18 @@
+import React from 'react'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import pokebolas from './components/pokebolas';
 
 
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <NavBar/>
     </div>
-  );
+  )
 }
 
-export default App;
+
+
--- proyecto-entrega/src/components/NavBar.js
+++ proyecto-entrega/src/components/NavBar.js
@@ -5,18 +5,32 @@ export default class NavBar extends Component{
   render(){
       return(
           <div>
-          <Navbar bg="light" expand="lg">
-            <Container>
-              <Navbar.Brand href="#home">La tienda de Oak!</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="/home">Home</Nav.Link>
-                  <Nav.Link href="/pokebolas">Pokebolas</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">La tienda de Oak!</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/pokebolas">Pokebolas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Held</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Battle</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+          
           </div>
     )
   }
